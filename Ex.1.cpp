f44dce4c6b7524714242cfb491ee95f6b6a41e17
Karine Lamonica <61923812+KarineLamonica@users.noreply.github.com>
2020-12-10 18:42:53 -0300

    Add files via upload

---
+++ Ex.1.cpp
@@ -0,0 +1,22 @@
+//Faça um algoritmo que armazene um vetor de inteiros de 20 posições, 
+//identifique o percentual de números ímpares e de números pares informados.
+
+#include <stdio.h>
+
+int main () {
+	
+	int i, V[20], Par[10], Impar[10], num;
+	
+	printf ("Escreva 20 numeros: ");
+	
+	for (i=0; i<20; i++) {
+		scanf ("%d", &num);
+	}
+	
+	
+
+	
+	
+	return 0;
+	
+}
